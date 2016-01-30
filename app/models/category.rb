137caedf12afde532b27a89a6efd1ef4e25058b7
Miguel <brazzoduro26@gmail.com>
2016-01-30 17:53:08 -0430

    Arreglando Relaciones de las tablas

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-	has_many :meme
+	has_many :memes
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ActiveRecord::Base
 	has_many :tagdetails
+	has_many :memes through: :tagdetails
 end
