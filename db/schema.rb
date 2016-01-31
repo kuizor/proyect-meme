d8e358c427830037388b84f1ef60f2e557538cf6
Miguel <brazzoduro26@gmail.com>
2016-01-31 00:27:17 -0430

    Creacion de la BD-modif

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131035031) do
+ActiveRecord::Schema.define(version: 20160131045551) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
